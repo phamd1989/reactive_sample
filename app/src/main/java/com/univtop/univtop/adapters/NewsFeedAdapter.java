6b673eeaa29a58b8dc9ab5a40cc02e3c8326f1dd
Dunca <phamd1989@gmail.com>
2015-11-10 00:59:34 -0800

    add following buttons

--- app/src/main/java/com/univtop/univtop/adapters/NewsFeedAdapter.java
+++ app/src/main/java/com/univtop/univtop/adapters/NewsFeedAdapter.java
@@ -1,12 +1,14 @@
 package com.univtop.univtop.adapters;
 
-import android.app.Activity;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.facebook.drawee.view.SimpleDraweeView;
@@ -14,8 +16,6 @@ import com.univtop.univtop.R;
 import com.univtop.univtop.models.Question;
 import com.univtop.univtop.services.APIService;
 import com.univtop.univtop.services.PagingApiService;
-
-import org.w3c.dom.Text;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -26,6 +26,8 @@ import rx.schedulers.Schedulers;
  * Created by dungpham on 11/1/15.
  */
 public class NewsFeedAdapter extends PageableListAdapter<Question> {
+
+    private boolean mIsFollowed = false;
 
     public NewsFeedAdapter(Context context) {
         super(context);
@@ -47,8 +49,10 @@ public class NewsFeedAdapter extends PageableListAdapter<Question> {
         View upvote;
         @Bind(R.id.answer_tv)
         TextView answers;
-        @Bind(R.id.follow_tv)
-        TextView follows;
+        @Bind(R.id.follow_btn)
+        ImageView followBtn;
+        @Bind(R.id.upvotes_tv)
+        TextView upvotes;
 
         public ViewHolder(View v) {
             super(v);
@@ -73,8 +77,22 @@ public class NewsFeedAdapter extends PageableListAdapter<Question> {
         vh.questionTitle.setText(question.getQuestionTitle());
         vh.questionDetails.setText(question.getQuestionDetails());
         vh.answers.setText(Integer.toString(question.getAnswers()) + " Answers");
-        vh.follows.setText(Integer.toString(question.getVotes()) + " Votes");
+        if (question.getVotes() > 0) {
+            vh.upvotes.setText(Integer.toString(question.getVotes()));
+        }
         vh.profilePic.setImageURI(Uri.parse(question.getUser().getAvatar()));
+        vh.followBtn.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+            @Override
+            public void onClick(View v) {
+                if (mIsFollowed) {
+                    vh.followBtn.setBackground(mContext.getResources().getDrawable(R.drawable.selector_profile_follow));
+                } else {
+                    vh.followBtn.setBackground(mContext.getResources().getDrawable(R.drawable.selector_profile_following));
+                }
+                mIsFollowed = !mIsFollowed;
+            }
+        });
     }
 
     @Override
